20aa8027bfb56789b5f455c9c152736dcbfa1ee3
Eformeh-Bello Wisdom <67350011+eformeh@users.noreply.github.com>
2025-08-27 13:27:48 +0100

    Add src/main/java/qucoon/mod/SpringServerless/model/request/TransactionCategoryCreateRequest.java via Qoonity

---
+++ src/main/java/qucoon/mod/SpringServerless/model/request/TransactionCategoryCreateRequest.java
@@ -0,0 +1,14 @@
+package qucoon.mod.SpringServerless.model.request;
+
+import lombok.Data;
+import javax.validation.constraints.NotEmpty;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class TransactionCategoryCreateRequest {
+public String transactionCategoryDescription;
+public String transactionCategoryName;
+public String transactionCategoryTransactionNature;
+}
