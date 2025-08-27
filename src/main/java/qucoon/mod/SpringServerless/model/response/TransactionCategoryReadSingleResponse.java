533dc9fd3a6cfc696e335d47c1f16e55de0d032e
Eformeh-Bello Wisdom <67350011+eformeh@users.noreply.github.com>
2025-08-27 13:30:27 +0100

    Add src/main/java/qucoon/mod/SpringServerless/model/response/TransactionCategoryReadSingleResponse.java via Qoonity

---
+++ src/main/java/qucoon/mod/SpringServerless/model/response/TransactionCategoryReadSingleResponse.java
@@ -0,0 +1,14 @@
+
+package qucoon.mod.SpringServerless.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import qucoon.mod.SpringServerless.model.entity.*;
+
+@Data
+@AllArgsConstructor
+public class TransactionCategoryReadSingleResponse {
+    public String responseCode;
+    public String responseMessage;
+    public TransactionCategory data;
+}
