06e3ed1249cfaf487ed3e4c16ea48cfc73a5d8d9
Eformeh-Bello Wisdom <67350011+eformeh@users.noreply.github.com>
2025-08-27 13:24:48 +0100

    Add src/main/java/qucoon/mod/SpringServerless/model/request/DepartmentCreateRequest.java via Qoonity

---
+++ src/main/java/qucoon/mod/SpringServerless/model/request/DepartmentCreateRequest.java
@@ -0,0 +1,13 @@
+package qucoon.mod.SpringServerless.model.request;
+
+import lombok.Data;
+import javax.validation.constraints.NotEmpty;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class DepartmentCreateRequest {
+public String departmentDescription;
+public String departmentName;
+}
