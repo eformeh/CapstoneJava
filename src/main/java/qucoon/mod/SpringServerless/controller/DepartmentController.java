7371a48ab512fcb579ffcfdd66b41b58b28acea3
Eformeh-Bello Wisdom <67350011+eformeh@users.noreply.github.com>
2025-08-27 13:21:36 +0100

    Add src/main/java/qucoon/mod/SpringServerless/controller/DepartmentController.java via Qoonity

---
+++ src/main/java/qucoon/mod/SpringServerless/controller/DepartmentController.java
@@ -0,0 +1,271 @@
+
+package qucoon.mod.SpringServerless.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import qucoon.mod.SpringServerless.utility.model.response.BaseResponse;
+import qucoon.mod.SpringServerless.model.request.*;
+import qucoon.mod.SpringServerless.model.response.*;
+import qucoon.mod.SpringServerless.service.*;
+import qucoon.mod.SpringServerless.utility.Environment;
+import qucoon.mod.SpringServerless.utility.JwtUtility;
+import qucoon.mod.SpringServerless.utility.constant.PrivilegeConstant;
+import qucoon.mod.SpringServerless.utility.model.request.ModulePrivilege;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/department")
+@Validated
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+    private final Environment environment;
+
+    public DepartmentController(DepartmentService departmentService,Environment environment) {
+        this.departmentService = departmentService;
+        this.environment = environment;
+    }
+
+
+@PostMapping("/create")
+public BaseResponse create(
+@RequestHeader("Authorization") String authorization,
+@Valid @RequestBody DepartmentCreateRequest request) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.CREATE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return departmentService.create(request);
+}
+
+
+@PostMapping("/bulk-create")
+public BaseResponse bulkCreate(
+@RequestHeader("Authorization") String authorization,
+        @Valid @RequestBody List<DepartmentCreateRequest> request
+        ) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.CREATE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return departmentService.bulkCreate(request);
+}
+
+@PostMapping("/update")
+public BaseResponse update(
+@RequestHeader("Authorization") String authorization,
+@Valid @RequestBody DepartmentUpdateRequest request) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.UPDATE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return departmentService.update(request);
+}
+
+@GetMapping("/read")
+public DepartmentReadListResponse read(
+@RequestHeader("Authorization") String authorization
+) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return departmentService.read();
+}
+
+@DeleteMapping("/delete/{departmentId}")
+public BaseResponse softDeleteDepartment(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int  departmentId) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.DELETE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return departmentService.delete(departmentId);
+}
+
+
+
+@GetMapping("/read-by-department-created-at/{departmentCreatedAt}")
+public DepartmentReadListResponse getDepartmentDepartmentCreatedAt(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String departmentCreatedAt) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return departmentService.readByDepartmentCreatedAt(departmentCreatedAt);
+}
+
+
+@GetMapping("/read-by-department-description/{departmentDescription}")
+public DepartmentReadListResponse getDepartmentDepartmentDescription(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String departmentDescription) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return departmentService.readByDepartmentDescription(departmentDescription);
+}
+
+
+@GetMapping("/read-by-department-id/{departmentId}")
+public DepartmentReadSingleResponse getDepartmentDepartmentId(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int departmentId) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return departmentService.readByDepartmentId(departmentId);
+}
+
+
+@GetMapping("/read-by-department-name/{departmentName}")
+public DepartmentReadListResponse getDepartmentDepartmentName(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String departmentName) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return departmentService.readByDepartmentName(departmentName);
+}
+
+
+@GetMapping("/read-by-department-status/{departmentStatus}")
+public DepartmentReadListResponse getDepartmentDepartmentStatus(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String departmentStatus) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return departmentService.readByDepartmentStatus(departmentStatus);
+}
+
+
+@GetMapping("/read-by-department-updated-at/{departmentUpdatedAt}")
+public DepartmentReadListResponse getDepartmentDepartmentUpdatedAt(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String departmentUpdatedAt) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return departmentService.readByDepartmentUpdatedAt(departmentUpdatedAt);
+}
+
+@GetMapping("/search")
+public DepartmentReadPagedResponse search(
+@RequestHeader("Authorization") String authorization,
+
+        @RequestParam(defaultValue = "0") int pageNumber,
+        @RequestParam(defaultValue = "10") int pageSize,
+        @RequestParam(defaultValue = "departmentId") String sortBy,
+        @RequestParam(defaultValue = "DESC") String sortDir,
+        @RequestParam(required = false) String search,
+
+        @RequestParam(required = false) String departmentCreatedAt,
+        @RequestParam(required = false) String departmentDescription,
+        @RequestParam(required = false) Integer departmentId,
+        @RequestParam(required = false) String departmentName,
+        @RequestParam(required = false) String departmentStatus,
+        @RequestParam(required = false) String departmentUpdatedAt
+) {
+    DepartmentFilterRequest filter = new DepartmentFilterRequest();
+    filter.setPageNumber(pageNumber);
+    filter.setPageSize(pageSize);
+    filter.setSortBy(sortBy);
+    filter.setSortDir(sortDir);
+    filter.setSearch(search);
+
+    filter.setDepartmentCreatedAt(departmentCreatedAt);
+    filter.setDepartmentDescription(departmentDescription);
+    filter.setDepartmentId(departmentId);
+    filter.setDepartmentName(departmentName);
+    filter.setDepartmentStatus(departmentStatus);
+    filter.setDepartmentUpdatedAt(departmentUpdatedAt);
+
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return departmentService.search(filter);
+}
+}
