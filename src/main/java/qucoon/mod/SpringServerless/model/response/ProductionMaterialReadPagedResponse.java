f268bea3902cd714820613a1c32ca269c5ad8ad5
Eformeh-Bello Wisdom <67350011+eformeh@users.noreply.github.com>
2025-08-27 13:29:52 +0100

    Add src/main/java/qucoon/mod/SpringServerless/model/response/ProductionMaterialReadPagedResponse.java via Qoonity

---
+++ src/main/java/qucoon/mod/SpringServerless/model/response/ProductionMaterialReadPagedResponse.java
@@ -0,0 +1,18 @@
+package qucoon.mod.SpringServerless.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import qucoon.mod.SpringServerless.model.dto.ProductionMaterialDto;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class ProductionMaterialReadPagedResponse {
+    private String responseCode;
+    private String responseMessage;
+    private List<ProductionMaterialDto> data;
+    private long totalRecords;
+    private int pageNumber;
+    private int pageSize;
+    private int totalPages;
+}
