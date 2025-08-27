67172a3abe279ac8a235ef4b71beff0f1f0f892b
Eformeh-Bello Wisdom <67350011+eformeh@users.noreply.github.com>
2025-08-27 13:21:45 +0100

    Add src/main/java/qucoon/mod/SpringServerless/controller/JobController.java via Qoonity

---
+++ src/main/java/qucoon/mod/SpringServerless/controller/JobController.java
@@ -0,0 +1,271 @@
+
+package qucoon.mod.SpringServerless.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import qucoon.mod.SpringServerless.utility.model.response.BaseResponse;
+import qucoon.mod.SpringServerless.model.request.*;
+import qucoon.mod.SpringServerless.model.response.*;
+import qucoon.mod.SpringServerless.service.*;
+import qucoon.mod.SpringServerless.utility.Environment;
+import qucoon.mod.SpringServerless.utility.JwtUtility;
+import qucoon.mod.SpringServerless.utility.constant.PrivilegeConstant;
+import qucoon.mod.SpringServerless.utility.model.request.ModulePrivilege;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/job")
+@Validated
+public class JobController {
+
+    private final JobService jobService;
+    private final Environment environment;
+
+    public JobController(JobService jobService,Environment environment) {
+        this.jobService = jobService;
+        this.environment = environment;
+    }
+
+
+@PostMapping("/create")
+public BaseResponse create(
+@RequestHeader("Authorization") String authorization,
+@Valid @RequestBody JobCreateRequest request) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.CREATE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return jobService.create(request);
+}
+
+
+@PostMapping("/bulk-create")
+public BaseResponse bulkCreate(
+@RequestHeader("Authorization") String authorization,
+        @Valid @RequestBody List<JobCreateRequest> request
+        ) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.CREATE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return jobService.bulkCreate(request);
+}
+
+@PostMapping("/update")
+public BaseResponse update(
+@RequestHeader("Authorization") String authorization,
+@Valid @RequestBody JobUpdateRequest request) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.UPDATE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return jobService.update(request);
+}
+
+@GetMapping("/read")
+public JobReadListResponse read(
+@RequestHeader("Authorization") String authorization
+) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return jobService.read();
+}
+
+@DeleteMapping("/delete/{jobId}")
+public BaseResponse softDeleteJob(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int  jobId) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.DELETE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return jobService.delete(jobId);
+}
+
+
+
+@GetMapping("/read-by-job-created-at/{jobCreatedAt}")
+public JobReadListResponse getJobJobCreatedAt(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String jobCreatedAt) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return jobService.readByJobCreatedAt(jobCreatedAt);
+}
+
+
+@GetMapping("/read-by-job-department-id/{jobDepartmentId}")
+public JobReadListResponse getJobJobDepartmentId(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int jobDepartmentId) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return jobService.readByJobDepartmentId(jobDepartmentId);
+}
+
+
+@GetMapping("/read-by-job-id/{jobId}")
+public JobReadSingleResponse getJobJobId(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int jobId) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return jobService.readByJobId(jobId);
+}
+
+
+@GetMapping("/read-by-job-role-id/{jobRoleId}")
+public JobReadListResponse getJobJobRoleId(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int jobRoleId) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return jobService.readByJobRoleId(jobRoleId);
+}
+
+
+@GetMapping("/read-by-job-status/{jobStatus}")
+public JobReadListResponse getJobJobStatus(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String jobStatus) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return jobService.readByJobStatus(jobStatus);
+}
+
+
+@GetMapping("/read-by-job-updated-at/{jobUpdatedAt}")
+public JobReadListResponse getJobJobUpdatedAt(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String jobUpdatedAt) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return jobService.readByJobUpdatedAt(jobUpdatedAt);
+}
+
+@GetMapping("/search")
+public JobReadPagedResponse search(
+@RequestHeader("Authorization") String authorization,
+
+        @RequestParam(defaultValue = "0") int pageNumber,
+        @RequestParam(defaultValue = "10") int pageSize,
+        @RequestParam(defaultValue = "jobId") String sortBy,
+        @RequestParam(defaultValue = "DESC") String sortDir,
+        @RequestParam(required = false) String search,
+
+        @RequestParam(required = false) String jobCreatedAt,
+        @RequestParam(required = false) Integer jobDepartmentId,
+        @RequestParam(required = false) Integer jobId,
+        @RequestParam(required = false) Integer jobRoleId,
+        @RequestParam(required = false) String jobStatus,
+        @RequestParam(required = false) String jobUpdatedAt
+) {
+    JobFilterRequest filter = new JobFilterRequest();
+    filter.setPageNumber(pageNumber);
+    filter.setPageSize(pageSize);
+    filter.setSortBy(sortBy);
+    filter.setSortDir(sortDir);
+    filter.setSearch(search);
+
+    filter.setJobCreatedAt(jobCreatedAt);
+    filter.setJobDepartmentId(jobDepartmentId);
+    filter.setJobId(jobId);
+    filter.setJobRoleId(jobRoleId);
+    filter.setJobStatus(jobStatus);
+    filter.setJobUpdatedAt(jobUpdatedAt);
+
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return jobService.search(filter);
+}
+}
