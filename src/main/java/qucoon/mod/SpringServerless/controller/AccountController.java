6e2bc86134ce1b01170b84d64cf4f105d96c3de7
Eformeh-Bello Wisdom <67350011+eformeh@users.noreply.github.com>
2025-08-27 13:21:25 +0100

    Add src/main/java/qucoon/mod/SpringServerless/controller/AccountController.java via Qoonity

---
+++ src/main/java/qucoon/mod/SpringServerless/controller/AccountController.java
@@ -0,0 +1,291 @@
+
+package qucoon.mod.SpringServerless.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import qucoon.mod.SpringServerless.utility.model.response.BaseResponse;
+import qucoon.mod.SpringServerless.model.request.*;
+import qucoon.mod.SpringServerless.model.response.*;
+import qucoon.mod.SpringServerless.service.*;
+import qucoon.mod.SpringServerless.utility.Environment;
+import qucoon.mod.SpringServerless.utility.JwtUtility;
+import qucoon.mod.SpringServerless.utility.constant.PrivilegeConstant;
+import qucoon.mod.SpringServerless.utility.model.request.ModulePrivilege;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/account")
+@Validated
+public class AccountController {
+
+    private final AccountService accountService;
+    private final Environment environment;
+
+    public AccountController(AccountService accountService,Environment environment) {
+        this.accountService = accountService;
+        this.environment = environment;
+    }
+
+
+@PostMapping("/create")
+public BaseResponse create(
+@RequestHeader("Authorization") String authorization,
+@Valid @RequestBody AccountCreateRequest request) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.CREATE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.create(request);
+}
+
+
+@PostMapping("/bulk-create")
+public BaseResponse bulkCreate(
+@RequestHeader("Authorization") String authorization,
+        @Valid @RequestBody List<AccountCreateRequest> request
+        ) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.CREATE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.bulkCreate(request);
+}
+
+@PostMapping("/update")
+public BaseResponse update(
+@RequestHeader("Authorization") String authorization,
+@Valid @RequestBody AccountUpdateRequest request) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.UPDATE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.update(request);
+}
+
+@GetMapping("/read")
+public AccountReadListResponse read(
+@RequestHeader("Authorization") String authorization
+) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.read();
+}
+
+@DeleteMapping("/delete/{accountId}")
+public BaseResponse softDeleteAccount(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int  accountId) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.DELETE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.delete(accountId);
+}
+
+
+
+@GetMapping("/read-by-account-account-name/{accountAccountName}")
+public AccountReadListResponse getAccountAccountAccountName(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String accountAccountName) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.readByAccountAccountName(accountAccountName);
+}
+
+
+@GetMapping("/read-by-account-account-type-id/{accountAccountTypeId}")
+public AccountReadListResponse getAccountAccountAccountTypeId(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int accountAccountTypeId) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.readByAccountAccountTypeId(accountAccountTypeId);
+}
+
+
+@GetMapping("/read-by-account-created-at/{accountCreatedAt}")
+public AccountReadListResponse getAccountAccountCreatedAt(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String accountCreatedAt) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.readByAccountCreatedAt(accountCreatedAt);
+}
+
+
+@GetMapping("/read-by-account-description/{accountDescription}")
+public AccountReadListResponse getAccountAccountDescription(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String accountDescription) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.readByAccountDescription(accountDescription);
+}
+
+
+@GetMapping("/read-by-account-id/{accountId}")
+public AccountReadSingleResponse getAccountAccountId(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int accountId) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.readByAccountId(accountId);
+}
+
+
+@GetMapping("/read-by-account-status/{accountStatus}")
+public AccountReadListResponse getAccountAccountStatus(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String accountStatus) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.readByAccountStatus(accountStatus);
+}
+
+
+@GetMapping("/read-by-account-updated-at/{accountUpdatedAt}")
+public AccountReadListResponse getAccountAccountUpdatedAt(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String accountUpdatedAt) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.readByAccountUpdatedAt(accountUpdatedAt);
+}
+
+@GetMapping("/search")
+public AccountReadPagedResponse search(
+@RequestHeader("Authorization") String authorization,
+
+        @RequestParam(defaultValue = "0") int pageNumber,
+        @RequestParam(defaultValue = "10") int pageSize,
+        @RequestParam(defaultValue = "accountId") String sortBy,
+        @RequestParam(defaultValue = "DESC") String sortDir,
+        @RequestParam(required = false) String search,
+
+        @RequestParam(required = false) String accountAccountName,
+        @RequestParam(required = false) Integer accountAccountTypeId,
+        @RequestParam(required = false) String accountCreatedAt,
+        @RequestParam(required = false) String accountDescription,
+        @RequestParam(required = false) Integer accountId,
+        @RequestParam(required = false) String accountStatus,
+        @RequestParam(required = false) String accountUpdatedAt
+) {
+    AccountFilterRequest filter = new AccountFilterRequest();
+    filter.setPageNumber(pageNumber);
+    filter.setPageSize(pageSize);
+    filter.setSortBy(sortBy);
+    filter.setSortDir(sortDir);
+    filter.setSearch(search);
+
+    filter.setAccountAccountName(accountAccountName);
+    filter.setAccountAccountTypeId(accountAccountTypeId);
+    filter.setAccountCreatedAt(accountCreatedAt);
+    filter.setAccountDescription(accountDescription);
+    filter.setAccountId(accountId);
+    filter.setAccountStatus(accountStatus);
+    filter.setAccountUpdatedAt(accountUpdatedAt);
+
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return accountService.search(filter);
+}
+}
