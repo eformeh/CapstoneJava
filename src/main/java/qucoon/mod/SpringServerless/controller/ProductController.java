1d35d442d532d597262a2ad8b07137fda88913f0
Eformeh-Bello Wisdom <67350011+eformeh@users.noreply.github.com>
2025-08-27 13:21:57 +0100

    Add src/main/java/qucoon/mod/SpringServerless/controller/ProductController.java via Qoonity

---
+++ src/main/java/qucoon/mod/SpringServerless/controller/ProductController.java
@@ -0,0 +1,391 @@
+
+package qucoon.mod.SpringServerless.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import qucoon.mod.SpringServerless.utility.model.response.BaseResponse;
+import qucoon.mod.SpringServerless.model.request.*;
+import qucoon.mod.SpringServerless.model.response.*;
+import qucoon.mod.SpringServerless.service.*;
+import qucoon.mod.SpringServerless.utility.Environment;
+import qucoon.mod.SpringServerless.utility.JwtUtility;
+import qucoon.mod.SpringServerless.utility.constant.PrivilegeConstant;
+import qucoon.mod.SpringServerless.utility.model.request.ModulePrivilege;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+@Validated
+public class ProductController {
+
+    private final ProductService productService;
+    private final Environment environment;
+
+    public ProductController(ProductService productService,Environment environment) {
+        this.productService = productService;
+        this.environment = environment;
+    }
+
+
+@PostMapping("/create")
+public BaseResponse create(
+@RequestHeader("Authorization") String authorization,
+@Valid @RequestBody ProductCreateRequest request) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.CREATE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.create(request);
+}
+
+
+@PostMapping("/bulk-create")
+public BaseResponse bulkCreate(
+@RequestHeader("Authorization") String authorization,
+        @Valid @RequestBody List<ProductCreateRequest> request
+        ) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.CREATE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.bulkCreate(request);
+}
+
+@PostMapping("/update")
+public BaseResponse update(
+@RequestHeader("Authorization") String authorization,
+@Valid @RequestBody ProductUpdateRequest request) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.UPDATE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.update(request);
+}
+
+@GetMapping("/read")
+public ProductReadListResponse read(
+@RequestHeader("Authorization") String authorization
+) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.read();
+}
+
+@DeleteMapping("/delete/{productId}")
+public BaseResponse softDeleteProduct(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int  productId) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.DELETE, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.delete(productId);
+}
+
+
+
+@GetMapping("/read-by-product-barcode/{productBarcode}")
+public ProductReadListResponse getProductProductBarcode(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String productBarcode) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.readByProductBarcode(productBarcode);
+}
+
+
+@GetMapping("/read-by-product-category/{productCategory}")
+public ProductReadListResponse getProductProductCategory(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String productCategory) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.readByProductCategory(productCategory);
+}
+
+
+@GetMapping("/read-by-product-created-at/{productCreatedAt}")
+public ProductReadListResponse getProductProductCreatedAt(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String productCreatedAt) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.readByProductCreatedAt(productCreatedAt);
+}
+
+
+@GetMapping("/read-by-product-description/{productDescription}")
+public ProductReadListResponse getProductProductDescription(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String productDescription) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.readByProductDescription(productDescription);
+}
+
+
+@GetMapping("/read-by-product-id/{productId}")
+public ProductReadSingleResponse getProductProductId(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int productId) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.readByProductId(productId);
+}
+
+
+@GetMapping("/read-by-product-minimum-threshold/{productMinimumThreshold}")
+public ProductReadListResponse getProductProductMinimumThreshold(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int productMinimumThreshold) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.readByProductMinimumThreshold(productMinimumThreshold);
+}
+
+
+@GetMapping("/read-by-product-name/{productName}")
+public ProductReadListResponse getProductProductName(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String productName) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.readByProductName(productName);
+}
+
+
+@GetMapping("/read-by-product-price/{productPrice}")
+public ProductReadListResponse getProductProductPrice(
+@RequestHeader("Authorization") String authorization,
+@PathVariable Double productPrice) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.readByProductPrice(productPrice);
+}
+
+
+@GetMapping("/read-by-product-s-k-u/{productSKU}")
+public ProductReadSingleResponse getProductProductSKU(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String productSKU) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.readByProductSKU(productSKU);
+}
+
+
+@GetMapping("/read-by-product-status/{productStatus}")
+public ProductReadListResponse getProductProductStatus(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String productStatus) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.readByProductStatus(productStatus);
+}
+
+
+@GetMapping("/read-by-product-stock-quantity/{productStockQuantity}")
+public ProductReadListResponse getProductProductStockQuantity(
+@RequestHeader("Authorization") String authorization,
+@PathVariable int productStockQuantity) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.readByProductStockQuantity(productStockQuantity);
+}
+
+
+@GetMapping("/read-by-product-updated-at/{productUpdatedAt}")
+public ProductReadListResponse getProductProductUpdatedAt(
+@RequestHeader("Authorization") String authorization,
+@PathVariable String productUpdatedAt) {
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.readByProductUpdatedAt(productUpdatedAt);
+}
+
+@GetMapping("/search")
+public ProductReadPagedResponse search(
+@RequestHeader("Authorization") String authorization,
+
+        @RequestParam(defaultValue = "0") int pageNumber,
+        @RequestParam(defaultValue = "10") int pageSize,
+        @RequestParam(defaultValue = "productId") String sortBy,
+        @RequestParam(defaultValue = "DESC") String sortDir,
+        @RequestParam(required = false) String search,
+
+        @RequestParam(required = false) String productBarcode,
+        @RequestParam(required = false) String productCategory,
+        @RequestParam(required = false) String productCreatedAt,
+        @RequestParam(required = false) String productDescription,
+        @RequestParam(required = false) Integer productId,
+        @RequestParam(required = false) Integer productMinimumThreshold,
+        @RequestParam(required = false) String productName,
+        @RequestParam(required = false) Double productPrice,
+        @RequestParam(required = false) String productSKU,
+        @RequestParam(required = false) String productStatus,
+        @RequestParam(required = false) Integer productStockQuantity,
+        @RequestParam(required = false) String productUpdatedAt
+) {
+    ProductFilterRequest filter = new ProductFilterRequest();
+    filter.setPageNumber(pageNumber);
+    filter.setPageSize(pageSize);
+    filter.setSortBy(sortBy);
+    filter.setSortDir(sortDir);
+    filter.setSearch(search);
+
+    filter.setProductBarcode(productBarcode);
+    filter.setProductCategory(productCategory);
+    filter.setProductCreatedAt(productCreatedAt);
+    filter.setProductDescription(productDescription);
+    filter.setProductId(productId);
+    filter.setProductMinimumThreshold(productMinimumThreshold);
+    filter.setProductName(productName);
+    filter.setProductPrice(productPrice);
+    filter.setProductSKU(productSKU);
+    filter.setProductStatus(productStatus);
+    filter.setProductStockQuantity(productStockQuantity);
+    filter.setProductUpdatedAt(productUpdatedAt);
+
+JwtUtility jwt = new JwtUtility();
+String className = getClass().getSimpleName();
+String baseName  = className.endsWith("Controller")
+        ? className.substring(0, className.length() - "Controller".length())
+        : className;
+String snakeCase = baseName.replaceAll("([a-z])([A-Z])", "$1_$2")
+        .toLowerCase();
+String moduleKey = snakeCase.toUpperCase();
+ModulePrivilege privilege = new ModulePrivilege(PrivilegeConstant.READ, moduleKey);
+environment.userInfo =  jwt.privilegeAuthorization(privilege, authorization, LoginResponse.class);
+    return productService.search(filter);
+}
+}
